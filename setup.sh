56a3a190c1ae9c355244a09216dc31a3f9fdf66e
al3xdiaz <alexleonel96@hotmail.com>
2022-04-05 22:00:03 -0600

    feat: playbook nodejs

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 cat /home/ansible/ssh-keys/ansible.pub || ssh-keygen -b 3072 -t rsa -f /home/ansible/ssh-keys/ansible -q -N '' -C ansible
+echo "usuario: $HOST_USER"
+groupadd $HOST_USER && useradd -g $HOST_USER $HOST_USER && chown -R $HOST_USER:$HOST_USER /home/ansible/ssh-keys/ansible.pub && chown -R $HOST_USER:$HOST_USER /home/ansible/ssh-keys/ansible || echo "no se pudo crear tu usuario :c"
 exec $@
