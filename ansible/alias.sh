d5be8b3892c98166db8061c6590302852866171e
al3xdiaz <alexleonel96@hotmail.com>
2022-04-27 07:54:46 -0600

    fonts

--- ansible/alias.sh
+++ ansible/alias.sh
@@ -1,3 +1,4 @@
 alias ping="ansible all --key-file ssh-keys/ansible -m ping"
 alias playbook="ansible-playbook --key-file ssh-keys/ansible"
 alias hosts="ansible all --list-hosts"
+alias fonts="playbook playbooks/devtools.yml --tags fonts --ask-become-pass"
