c377dae9a9de42736eabdb3d5a7470a3cebb1b27
al3xdiaz <alexleonel96@hotmail.com>
2022-07-30 10:17:54 -0600

    feat: aws_kms_key

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,12 @@
 provider "aws"{
     region = "us-east-1"
 }
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "backend" {
   bucket = var.bucket_name
     tags = var.bucket_tags
@@ -20,7 +26,15 @@ resource "aws_s3_bucket_acl" "acl" {
     bucket = aws_s3_bucket.backend.id
     acl = var.bucket_alc
 }
-
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.backend.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
 resource "aws_instance" "test" {
     ami = var.image_id
     instance_type = var.instance_type
