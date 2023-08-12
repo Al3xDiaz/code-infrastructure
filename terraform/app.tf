128c36d488aeb39e0f93e7362defcb5e0d03c556
al3xdiaz <alexleonel96@hotmail.com>
2023-08-12 02:16:08 -0600

    [feat] bucket s3

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 0.12"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.38.0"
+      source = "hashicorp/aws"
+      version = "5.12.0"
     }
   }
 }
